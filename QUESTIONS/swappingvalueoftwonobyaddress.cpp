95bc1458d4198df4422274b362c9eaf36baaf18a
Seerat Grover <groseeru2205@gmail.com>
2025-02-08 23:51:11 +0530

    call by reference

---
+++ QUESTIONS/swappingvalueoftwonobyaddress.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// Function to swap two numbers using call by reference
+void swap(int &a, int &b) 
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int main() 
+{
+    int x, y;
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    // Display numbers before swap
+    cout << "Before swapping: " << "x = " << x << ", y = " << y << endl;
+    // Call swap function
+    swap(x, y);
+    // Display numbers after swap
+    cout << "After swapping: " << "x = " << x << ", y = " << y << endl;
+    return 0;
+}
